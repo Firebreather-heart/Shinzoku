6e65caf55d3e606e3080b560f14bb90700b8d725
firebreather-heart <dtenny95@gmail.com>
2025-05-14 05:08:09 +0100

    updated shop

--- shinzoku-UI/next.config.js
+++ shinzoku-UI/next.config.js
@@ -1,4 +1,16 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['res.cloudinary.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                pathname: '/**',
+            },
+        ],
+    },
     typescript: {
         // Disable type checking in Next.js (run manually with `tsc --noEmit`)
         ignoreBuildErrors: false
@@ -7,4 +19,6 @@ module.exports = {
         // Disable ESLint during dev builds to reduce RAM
         ignoreDuringBuilds: true
     }
-};
+}
+
+module.exports = nextConfig
